84859e7022cf76ed42f407cf2fe5e3be5a0e5296
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2022-07-31 08:33:18 +0900

    feat: add robots meta tag

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,19 @@
 import type { AppProps } from "next/app";
+import Head from "next/head";
 import { ChakraProvider } from "@chakra-ui/react";
 import theme from "../theme";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider theme={theme}>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <>
+      <Head>
+        <meta name="robots" content="noindex,nofollow,noarchive" />
+      </Head>
+
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </>
   );
 }
 
