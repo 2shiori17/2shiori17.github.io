a1d1e85c784b897662f952ab222d98b33b966ef2
2shiori17 <98276492+2shiori17@users.noreply.github.com>
2022-08-07 12:17:32 +0900

    fix: fix display corruption on mobile

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -5,7 +5,6 @@ import {
   forwardRef,
   Box,
   BoxProps,
-  Center,
   Container,
   Flex,
   Heading,
@@ -13,7 +12,6 @@ import {
   Icon,
   Link,
   Spacer,
-  Text,
 } from "@chakra-ui/react";
 import { FaTwitter, FaGithub, FaKeybase } from "react-icons/fa";
 
@@ -45,15 +43,16 @@ const Header = forwardRef<BoxProps, "div">((props, ref) => (
   </Box>
 ));
 
-const Footer = forwardRef<BoxProps, "div">((props, ref) => (
-  <Box ref={ref} {...props}>
-    <Container maxWidth="container.md" paddingY={4}>
-      <Center>
-        <Text>&copy; {new Date().getFullYear()} 2shiori17</Text>
-      </Center>
-    </Container>
-  </Box>
-));
+// TODO: implement sticky footer
+// const Footer = forwardRef<BoxProps, "div">((props, ref) => (
+//   <Box ref={ref} {...props}>
+//     <Container maxWidth="container.md" paddingY={4}>
+//       <Center>
+//         <Text>&copy; {new Date().getFullYear()} 2shiori17</Text>
+//       </Center>
+//     </Container>
+//   </Box>
+// ));
 
 export interface LayoutProps {
   children: ReactNode;
@@ -66,13 +65,11 @@ export function Layout(props: LayoutProps) {
         <meta name="robots" content="noindex,nofollow,noarchive" />
       </Head>
 
-      <Box minHeight="100vh" display="grid" gridTemplateRows="auto 1fr auto">
-        <Header as="header" borderBottomWidth={1} />
-        <Container as="main" maxWidth="container.md" marginY={8}>
-          {props.children}
-        </Container>
-        <Footer as="footer" borderTopWidth={1} />
-      </Box>
+      <Header as="header" borderBottomWidth={1} />
+      <Container as="main" maxWidth="container.md" marginY={8}>
+        {props.children}
+      </Container>
+      {/* <Footer as="footer" borderTopWidth={1} /> */}
     </>
   );
 }
